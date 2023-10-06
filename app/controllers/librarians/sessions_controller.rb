f8104b88f3a3c58aca8397b74afc2889492f0a9d
tendengango <framboutel@gmail.com>
2023-10-06 18:40:50 +0100

    second commit with admin pannel and user ok

--- app/controllers/librarians/sessions_controller.rb
+++ app/controllers/librarians/sessions_controller.rb
@@ -17,11 +17,13 @@ class Librarians::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-
+  def after_sign_in_path_for(resource)
+    '/librarians'
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  end
 end
